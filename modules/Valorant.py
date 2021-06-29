3ea12ad518f5819d3655932e3ec9af90baf456b5
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-06-29 15:23:17 +1000

    Moved to embeds for stats

--- modules/Valorant.py
+++ modules/Valorant.py
@@ -1,5 +1,6 @@
 from Player import Player
 import discord
+from discord import Embed
 from discord.ext import commands
 import Scraper
 from io import BytesIO
@@ -21,8 +22,6 @@ class Valorant(commands.Cog):
                 type = "competitive"
             elif type == "spike":
                 type = "spikerush"
-
-
 
             with BytesIO() as image_binary:
                 name = command.split('#')[0]
@@ -111,7 +110,34 @@ class Valorant(commands.Cog):
                 message += 'Flawless:     ' + str(player[1].game.flawless)          + '\n'
                 message += '```'
 
-                await context.send(message)
+                embed = Embed(color=0xfa4454)
+                embed.set_author(name=f"{name} {type} stats", url=player[1].url, icon_url=player[1].avatar)
+                embed.title=f"Playtime: {str(player[1].game.playtime)}"
+                embed.description=f"Matches: {str(player[1].game.matches)}"
+                
+                embed.add_field(name="Wins", value=str(player[1].game.wins), inline=True)
+                embed.add_field(name="Win Rate", value=str(player[1].game.winRate), inline=True)
+                embed.add_field(name="Score/Round", value=str(player[1].game.scorePerRound), inline=True)
+
+                embed.add_field(name="KDA", value=str(player[1].damage.kda), inline=True)
+                embed.add_field(name="KD", value=str(player[1].damage.kd), inline=True)
+                embed.add_field(name="Damage/Round", value=str(player[1].damage.dmg), inline=True)
+
+                embed.add_field(name="Kills", value=str(player[1].damage.kills) , inline=True)
+                embed.add_field(name="Deaths", value=str(player[1].damage.deaths) , inline=True)
+                embed.add_field(name="Assists", value=str(player[1].damage.assists) , inline=True)
+
+                embed.add_field(name="Kills/Round", value=str(player[1].damage.killsPerRound), inline=True)
+                embed.add_field(name="Most Kills", value=str(player[1].game.mostKills), inline=True)
+                embed.add_field(name="Headshots", value=str(player[1].damage.Headshots), inline=True)
+                embed.add_field(name="Clutches", value=str(player[1].game.clutch), inline=True)
+                embed.add_field(name="Flawless", value=str(player[1].game.flawless), inline=True)
+                embed.add_field(name="Headshot%", value=str(player[1].damage.headshotRate), inline=True)
+
+
+                await context.send(embed=embed)
+
+                #await context.send(message)
             elif (player[0] == 1):
                 await context.send("User not authenicated. Please authenticate " + player[1])
             elif (player[0] == 404):
