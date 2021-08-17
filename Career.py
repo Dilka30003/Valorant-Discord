3623f9165c0c2a7731a1a250788d7c7088c75378
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-08-17 17:29:55 +1000

    Added times and error handling

--- Career.py
+++ Career.py
@@ -1,5 +1,7 @@
 import requests
 import yaml
+from enum import Enum
+from datetime import datetime, timedelta
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
 
@@ -20,6 +22,8 @@ class Game():
         # Get the map and mode of the game
         self.map = data['map']
         self.mode = data['mode']
+        self.gameStart = datetime.fromtimestamp(data['game_start']/1000)
+        self.gameLength = timedelta(milliseconds=data['game_length'])
         
     
     def __players(self, data):
@@ -78,10 +82,27 @@ class Game():
 
 
 class Career():
+    class CODE(Enum):
+        ERROR = 0
+        DUPLICATE = 1
+        NOTFOUND = 2
+
+        INVALID = 404
+        NODATA = 204
+        OK = 200
+
     def __init__(self, name, tag):
         # Get Data (Will take a while)
         data = requests.get(f'https://api.henrikdev.xyz/valorant/v3/matches/ap/{name}/{tag}')
         # Do some error checking im too lazy to do
+        if data.status_code == self.CODE.INVALID.value:
+            self.isValid = False
+            return
+        elif data.status_code == self.CODE.NODATA.value:
+            self.isValid = False
+            return
+        self.isValid = True
+
         games = data.json()['data']
 
         # Iterate over every game (5 as of writing)
@@ -94,7 +115,7 @@ class Career():
         img = Image.new('RGBA', (1920, 256*len(self.GameList)), (255, 0, 0, 0))                           # Create the main image and fonts
         LargeFont = ImageFont.truetype("Roboto/Roboto-Medium.ttf", 100)
         subtextFont = ImageFont.truetype("Roboto/Roboto-Medium.ttf", 70)
-        posFont = ImageFont.truetype("Roboto/Roboto-Medium.ttf", 40)
+        miniFont = ImageFont.truetype("Roboto/Roboto-Medium.ttf", 40)
 
         MARGIN = 10
 
@@ -151,12 +172,12 @@ class Career():
             winSize = LargeFont.getsize(f"{game.roundWins}")
             colonSize = LargeFont.getsize(":")
 
-            draw.text((550, i*256 + 128-100), f"{game.roundWins}",(127,255,183),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
-            draw.text((550+winSize[0]+10, i*256 + 128-100), ":",(200,200,200),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
-            draw.text((550+winSize[0]+colonSize[0]+20, i*256 + 128-100), f"{game.roundLoss}",(255,88,90),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((600, i*256 + 128-100), f"{game.roundWins}",(127,255,183),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((600+winSize[0]+10, i*256 + 128-100), ":",(200,200,200),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((600+winSize[0]+colonSize[0]+20, i*256 + 128-100), f"{game.roundLoss}",(255,88,90),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
             
-            startPos = 600
-            endPos = 750
+            startPos = 650
+            endPos = 800
 
             # Position
             if game.position == 1:
@@ -166,27 +187,39 @@ class Career():
 
             draw.rounded_rectangle((startPos, i*256+150, endPos, i*256+200), fill=colour, outline=(0,0,0), width=1, radius=25)
 
-            posSize = posFont.getsize(f"{game.position}")
-            draw.text((startPos+(endPos-startPos-posSize[0])//2+3, i*256 + 150+(50-posSize[1])//2-3), f"{game.position}",(0,0,0),font=posFont)
+            posSize = miniFont.getsize(f"{game.position}")
+            draw.text((startPos+(endPos-startPos-posSize[0])//2+3, i*256 + 150+(50-posSize[1])//2-3), f"{game.position}",(0,0,0),font=miniFont)
 
             # ACS
             headingSize = subtextFont.getsize("ACS")
             scoreSize = LargeFont.getsize(f"{game.score//(game.roundWins+game.roundLoss)}")
 
-            draw.text((550+300 +scoreSize[0]-headingSize[0], i*256 + 43), "ACS",(200,200,200),font=subtextFont, stroke_width=1, stroke_fill=(0,0,0))
-            draw.text((550+300, i*256 + 70+43), f"{game.score//(game.roundWins+game.roundLoss)}",(255,255,255),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((550+400 +scoreSize[0]-headingSize[0], i*256 + 43), "ACS",(200,200,200),font=subtextFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((550+400, i*256 + 70+43), f"{game.score//(game.roundWins+game.roundLoss)}",(255,255,255),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
 
             # KDA
             headingSize = subtextFont.getsize("K/D/A")
             kdaSize = LargeFont.getsize(f"{game.k}/{game.d}/{game.a}")
 
-            draw.text((550+500+400-headingSize[0], i*256 + 43), "K/D/A",(200,200,200),font=subtextFont, stroke_width=1, stroke_fill=(0,0,0))
-            draw.text((550+500+400-kdaSize[0], i*256 + 70+43), f"{game.k}/{game.d}/{game.a}",(255,255,255),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((550+1050-headingSize[0], i*256 + 43), "K/D/A",(200,200,200),font=subtextFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((550+1050-kdaSize[0], i*256 + 70+43), f"{game.k}/{game.d}/{game.a}",(255,255,255),font=LargeFont, stroke_width=1, stroke_fill=(0,0,0))
+
+            # Date and Time
+            dateSize = miniFont.getsize(game.gameStart.strftime('%b %d'))
+            timeSize = miniFont.getsize(game.gameStart.strftime('%I:%M %p'))
+            lengthSize = miniFont.getsize(f'{game.gameLength.seconds//60}m {game.gameLength.seconds%60}s')
+
+            draw.text((img.width-dateSize[0]-20, i*256 + 20), game.gameStart.strftime('%b %d'),(255,255,255),font=miniFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((img.width-timeSize[0]-20, i*256 + 30 + dateSize[1]), game.gameStart.strftime('%I:%M %p'),(255,255,255),font=miniFont, stroke_width=1, stroke_fill=(0,0,0))
+            draw.text((img.width-lengthSize[0]-20, i*256 + 40 + dateSize[1] + timeSize[1]), f'{game.gameLength.seconds//60}m {game.gameLength.seconds%60}s',(255,255,255),font=miniFont, stroke_width=1, stroke_fill=(0,0,0))
+
 
         img.show()
             
 
 if __name__ == '__main__':
-    career = Career('8888', 'nadi')   # Create object for player career
-    career.Graphic()
-    pass
+    career = Career('Dilka30003', '0000')   # Create object for player career
+    if career.isValid:
+        career.Graphic()
+    else:
+        print('invalid')
